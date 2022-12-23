89b58b31ae2102de7e52af2a240400096c302c15
王梓城 <GW00284289@gwm.cn>
2022-12-23 16:51:29 +0800

    Initial commit

---
+++ src/main/java/com/example/pardata/PardataApplication.java
@@ -0,0 +1,13 @@
+package com.example.pardata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PardataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PardataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pardata/PardataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pardata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PardataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
